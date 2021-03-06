ad2e1a0a99c445ebb1f48471d36f0343a1b726ff
nikkkhil067 <emnikkhil@gmail.com>
2021-03-06 23:54:13 +0530

    Array Basic Problems

---
+++ Arrays/Basics/Inverse of Array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int* inverse(int* arr, int n){
+	int* inv = new int[n];
+	
+	for(int i=0;i<n;i++){
+		int pos = arr[i];
+		inv[pos] = i;
+	}
+	return inv;
+}
+
+void display(int* arr, int n){
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+int main(){
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	int* inv = inverse(arr, n);
+	display(inv, n);
+	return 0;
+}
+
